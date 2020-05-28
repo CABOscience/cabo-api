0f48ca3bd49b136e1ac7b0362bc69689e5534a24
Guillaume Larocque <glaroc@gmail.com>
2020-05-28 10:13:00 -0400

    Sesssion error

--- server/config/environment/globals.js
+++ server/config/environment/globals.js
@@ -7,7 +7,6 @@
 import path from 'path'
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-console.log(process.env.SESSIONSECRET)
 global.__ENV__ = process.env.NODE_ENV
 
 global.__DEV__ = __ENV__ === 'development'

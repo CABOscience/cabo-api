6b7237fda6e5b22fea8b792d16efb1926f34ee96
Guillaume Larocque <glaroc@gmail.com>
2020-06-12 16:29:18 -0400

    try query with mean

--- server/api/custom-endpoints.js
+++ server/api/custom-endpoints.js
@@ -1,5 +1,5 @@
 'use strict'
-import { Op } from 'sequelize'
+import { Op, QueryTypes } from 'sequelize'
 
 export default function (app, db) {
   //SEARCH SPECTRA BY TAXA
@@ -15,7 +15,12 @@ export default function (app, db) {
     }).then(ids => {
       res.send(ids)
     })
-  })
+  }),
+  //MEAN SPECTRA BY TAXA
+  app.get('/api/v1/leaf_spectra_mean/search/taxa', function (req, res) {
+    const results = await sequelize.query("SELECT wavelength, avg(r_t_average) as avg, min(r_t_average) as min, max(r_t_average) as max from spectra_processed WHERE scientific_name iLike '%"+req.query.taxa+"%' AND reflectance_transmittance='"+req.query.taxa+"' GROUP BY wavelength;", { type: sequelize.QueryTypes.SELECT }); 
+    res.send(results)
+  }
 }
 
 

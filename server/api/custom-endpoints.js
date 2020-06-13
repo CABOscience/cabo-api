01ffbb1389f509894bb39156e392c1798914b429
Guillaume Larocque <glaroc@gmail.com>
2020-06-13 09:11:19 -0400

    try query with mean

--- server/api/custom-endpoints.js
+++ server/api/custom-endpoints.js
@@ -1,4 +1,5 @@
 'use strict'
+import { Op } from 'sequelize'
 
 export default function (app, db) {
   //SEARCH SPECTRA BY TAXA
@@ -6,7 +7,7 @@ export default function (app, db) {
     db.LeafSpectra.findAll({
     where:{
       scientific_name : {
-        [db.Op.iLike]: '%'+req.query.taxa+'%'
+        [Op.iLike]: '%'+req.query.taxa+'%'
       },
       status: "submitted"
     },
@@ -17,7 +18,7 @@ export default function (app, db) {
   }),
   //MEAN SPECTRA BY TAXA
   app.get('/api/v1/leaf_spectra_mean/search/taxa', function (req, res) {
-    db.query("SELECT wavelength, avg(r_t_average) as avg, min(r_t_average) as min, max(r_t_average) as max from spectra_processed WHERE scientific_name iLike '%"+req.query.taxa+"%' AND reflectance_transmittance='"+req.query.type+"' GROUP BY wavelength;", { type: db.QueryTypes.SELECT }).then(ids => {
+    db.query("SELECT wavelength, avg(r_t_average) as avg, min(r_t_average) as min, max(r_t_average) as max from spectra_processed WHERE scientific_name iLike '%"+req.query.taxa+"%' AND reflectance_transmittance='"+req.query.type+"' GROUP BY wavelength ORDER BY wavelength;", { type: db.QueryTypes.SELECT }).then(ids => {
       res.send(ids);
     })
   })

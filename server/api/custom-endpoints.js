310b81db6dfcd9c09941ed6e35ef20f5dc9922b5
Guillaume Larocque <glaroc@gmail.com>
2020-06-16 14:30:02 -0400

    Mean spectra endpoints

--- server/api/custom-endpoints.js
+++ server/api/custom-endpoints.js
@@ -18,11 +18,11 @@ export default function (app, db) {
   }),
   //MEAN SPECTRA BY TAXA
   app.get('/api/v1/leaf_spectra_mean/search/', function (req, res) {
-    if(typeof req.query.taxa !== undefined){
+    if(typeof req.query.taxa !== 'undefined'){
       db.query("SELECT wavelength, avg(r_t_average) as avg, min(r_t_average) as min, max(r_t_average) as max from spectra_processed WHERE scientific_name iLike '%"+req.query.taxa+"%' AND reflectance_transmittance='"+req.query.type+"' GROUP BY wavelength ORDER BY wavelength;", { type: db.QueryTypes.SELECT }).then(result => {
         res.send(result);
       })
-    }else if(typeof req.query.ids !== undefined){
+    }else if(typeof req.query.ids !== 'undefined'){
       db.query("SELECT wavelength, avg(r_t_average) as avg, min(r_t_average) as min, max(r_t_average) as max from spectra_processed WHERE record_id IN("+req.query.ids+") AND reflectance_transmittance='"+req.query.type+"' GROUP BY wavelength ORDER BY wavelength;", { type: db.QueryTypes.SELECT }).then(result => {
         res.send(result);
       })

013fb520357ef967575dd093b2018e64f0b2c4b8
Guillaume Larocque <glaroc@gmail.com>
2020-06-18 11:38:55 -0400

    Use POST for mean spectra

--- server/api/custom-endpoints.js
+++ server/api/custom-endpoints.js
@@ -19,11 +19,11 @@ export default function (app, db) {
   //MEAN SPECTRA BY TAXA
   app.post('/api/v1/leaf_spectra_mean/search/', function (req, res) {
     if(typeof req.query.taxa !== 'undefined'){
-      db.query("SELECT wavelength, avg(r_t_average) as avg, min(r_t_average) as min, max(r_t_average) as max from spectra_processed WHERE scientific_name iLike '%"+req.query.taxa+"%' AND reflectance_transmittance='"+req.query.type+"' GROUP BY wavelength ORDER BY wavelength;", { type: db.QueryTypes.SELECT }).then(result => {
+      db.query("SELECT wavelength, avg(r_t_average) as avg, min(r_t_average) as min, max(r_t_average) as max from spectra_processed WHERE scientific_name iLike '%"+req.body.taxa+"%' AND reflectance_transmittance='"+req.body.type+"' GROUP BY wavelength ORDER BY wavelength;", { type: db.QueryTypes.SELECT }).then(result => {
         res.send(result);
       })
     }else if(typeof req.query.ids !== 'undefined'){
-      db.query("SELECT wavelength, avg(r_t_average) as avg, min(r_t_average) as min, max(r_t_average) as max from spectra_processed WHERE record_id IN("+req.query.ids+") AND reflectance_transmittance='"+req.query.type+"' GROUP BY wavelength ORDER BY wavelength;", { type: db.QueryTypes.SELECT }).then(result => {
+      db.query("SELECT wavelength, avg(r_t_average) as avg, min(r_t_average) as min, max(r_t_average) as max from spectra_processed WHERE record_id IN("+req.body.ids+") AND reflectance_transmittance='"+req.body.type+"' GROUP BY wavelength ORDER BY wavelength;", { type: db.QueryTypes.SELECT }).then(result => {
         res.send(result);
       })
     }

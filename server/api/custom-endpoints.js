bdfdb63bf8650be8527ed917b9d3099d133774a4
Guillaume Larocque <glaroc@gmail.com>
2020-06-25 17:20:38 -0400

    Link plants and bulk leaf samples

--- server/api/custom-endpoints.js
+++ server/api/custom-endpoints.js
@@ -11,7 +11,7 @@ export default function (app, db) {
       },
       status: "submitted"
     },
-    attributes : ['fulcrum_id','scientific_name', 'sample', 'geometry'],
+    attributes : ['fulcrum_id','scientific_name', 'sample_id', 'geometry'],
     }).then(ids => {
       res.send(ids)
     })

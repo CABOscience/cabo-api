9c0cd359fd22244214da8d6ae96571bf23aad56d
Guillaume Larocque <glaroc@gmail.com>
2020-09-25 14:47:53 -0400

    Site include

--- server/api/v1/relations.js
+++ server/api/v1/relations.js
@@ -7,8 +7,8 @@ export default function (db) {
 
   db.Sites.hasMany(db.Plots,{foreignKey: 'site_id'})
   db.Plots.hasMany(db.Plants,{foreignKey: 'plot_id'})
-  db.Sites.hasMany(db.Plants,{foreignKey: 'site', sourceKey:'fulcrum_id'})
-  db.Plants.belongsTo(db.Sites,{foreignKey: 'site', sourceKey:'fulcrum_id'})
+  db.Sites.hasMany(db.Plants,{as:'plants',foreignKey: 'site', sourceKey:'fulcrum_id'})
+  db.Plants.belongsTo(db.Sites,{as:'sites',foreignKey: 'site', sourceKey:'fulcrum_id'})
   //db.LeafSpectra.hasMany(db.LeafSpectraTransmittances, {as: 'transmittances', foreignKey: 'record_id'})
   //db.LeafSpectraData.belongsTo(db.LeafSpectra,{constraints: false})
   //db.LeafSpectraProcessed.belongsTo(db.LeafSpectra,{constraints: false})

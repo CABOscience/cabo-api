9869ee541861ecc21bde45b08e788e85a0fe67d6
Guillaume Larocque <glaroc@gmail.com>
2020-11-19 13:51:55 -0500

    Add Leaf Area and Water Samples

--- server/api/v1/LeafAreaAndWaterSamples/resource.js
+++ server/api/v1/LeafAreaAndWaterSamples/resource.js
@@ -4,7 +4,7 @@ import { Op } from 'sequelize'
 export default function (epilogue, db, resources) {
   const LeafAreaAndWaterSamplesResource = epilogue.resource({
     model: db.LeafAreaAndWaterSamples,
-    endpoints: ['Leaf_spectra_and_water_samples/', 'Leaf_spectra_and_water_samples/:fulcrum_id'],
+    endpoints: ['leaf_area_and_water_samples/', 'leaf_area_and_water_samples/:fulcrum_id'],
     search: [
       {operator: Op.eq, param: 'sample_id', attributes: [ 'sample_id' ]},
       {operator: Op.eq, param: 'created_at', attributes: ['created_at']},

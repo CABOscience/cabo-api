4b7a9edf761048e513c26ab67cd48875e2fe0cec
Guillaume Larocque <glaroc@gmail.com>
2020-11-20 12:41:36 -0500

    Add all traits endpoint

--- server/api/custom-endpoints.js
+++ server/api/custom-endpoints.js
@@ -81,9 +81,9 @@ export default function (app, db) {
     if(typeof req.query.trait !== 'undefined') {
       db.query("SELECT data_type FROM information_schema.columns where table_name = 'leaf_area_and_water_samples' AND column_name='"+req.query.trait+"'", { type: db.QueryTypes.SELECT }).then(type => {
         if(type[0]=='text'){
-          q="SELECT string_agg(substring("+req.query.trait+" from 0 for 7),',') as "+req.query.trait+" FROM leaf_area_and_water_samples WHERE status='submitted'";
+          let q="SELECT string_agg(substring("+req.query.trait+" from 0 for 7),',') as "+req.query.trait+" FROM leaf_area_and_water_samples WHERE status='submitted'";
         }else{
-          q="SELECT string_agg(substring("+req.query.trait+"::text from 0 for 7),',') as "+req.query.trait+" FROM leaf_area_and_water_samples WHERE status='submitted'";
+          let q="SELECT string_agg(substring("+req.query.trait+"::text from 0 for 7),',') as "+req.query.trait+" FROM leaf_area_and_water_samples WHERE status='submitted'";
         }
         db.query(q, { type: db.QueryTypes.SELECT }).then(result => {
           res.send(result);

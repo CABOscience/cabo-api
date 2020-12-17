eb5a92b97ad50c0fe946978591703662cd0639cd
Guillaume Larocque <glaroc@gmail.com>
2020-12-17 14:12:31 -0500

    Add spectra_leaves endpoint

--- server/api/custom-endpoints.js
+++ server/api/custom-endpoints.js
@@ -69,6 +69,16 @@ export default function (app, db) {
     }
   })
 
+  //MEAN SPECTRA BY TAXA
+  app.post('/api/v1/leaf_spectra_raw/search/', function (req, res) {
+    if(typeof req.body.ids !== 'undefined'){
+      db.query("SELECT wavelength, reflectance_transmittance, avg(r_t_average) as avg, min(r_t_average) as min, max(r_t_average) as max from spectra_processed WHERE sample_id IN("+req.body.ids+") GROUP BY wavelength, reflectance_transmittance ORDER BY wavelength;", { type: db.QueryTypes.SELECT }).then(result => {
+        res.send(result);
+      })
+    }
+  })
+
+
   app.get('/api/v1/leaf_spectra_mean/search/', function (req, res) {
     if(typeof req.query.species !== 'undefined'){
       db.query("SELECT scientific_name, reflectance_transmittance, wavelength, avg(r_t_average) as avg, min(r_t_average) as min, max(r_t_average) as max from spectra_processed WHERE scientific_name =  '"+req.query.species+"' GROUP BY scientific_name, wavelength, reflectance_transmittance ORDER BY scientific_name, wavelength;", { type: db.QueryTypes.SELECT }).then(result => {

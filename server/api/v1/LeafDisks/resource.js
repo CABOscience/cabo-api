a0bf8ceaf2e40e1c56ef2c57f36ed214632892f4
Guillaume Larocque <glaroc@gmail.com>
2021-04-23 17:45:13 -0400

    Add Pigments et al.

--- server/api/v1/LeafDisks/resource.js
+++ server/api/v1/LeafDisks/resource.js
@@ -9,7 +9,7 @@ export default function (epilogue, db, resources) {
       {operator: Op.eq, param: 'created_at', attributes: ['created_at']},
     ],
 	include: [{
-		model:db.PigmentsExtracts
+		model:db.Cryoboxes
 	}]
   })
   resources.LeafDisksResource = LeafDisksResource

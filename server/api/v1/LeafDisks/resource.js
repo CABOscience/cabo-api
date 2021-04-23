532881e96ab1a0ef8b28486d21c870286acec5ee
Guillaume Larocque <glaroc@gmail.com>
2021-04-23 17:48:14 -0400

    Add Pigments et al.

--- server/api/v1/LeafDisks/resource.js
+++ server/api/v1/LeafDisks/resource.js
@@ -3,13 +3,16 @@ import { Op } from 'sequelize'
 
 export default function (epilogue, db, resources) {
   const LeafDisksResource = epilogue.resource({
-    model: db.Pigments,
+    model: db.LeafDisks,
     endpoints: ['leaf_disks/', 'leaf_disks/:fulcrum_id'],
     search: [
       {operator: Op.eq, param: 'created_at', attributes: ['created_at']},
     ],
 	include: [{
 		model:db.Cryoboxes
+	},
+	{
+		model:db.PigmentsExtracts
 	}]
   })
   resources.LeafDisksResource = LeafDisksResource

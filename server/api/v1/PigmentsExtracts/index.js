dc8beeaf1b9e632f1d35febe974a59778d40bc74
Guillaume Larocque <glaroc@gmail.com>
2021-04-29 16:29:56 -0400

    Test hook

--- server/api/v1/PigmentsExtracts/index.js
+++ server/api/v1/PigmentsExtracts/index.js
@@ -53,7 +53,7 @@ export default function (db) {
     hooks: {
       afterFind: (l, options) => {
         for (var i = 0; i < l.length; i++) {
-          if(typeof l[i].leaf_disk.cryobox !== 'undefined' && l[i].leaf_disk.cryobox.preservation_method=='frozen') {
+          if(false && typeof l[i].leaf_disk.cryobox !== 'undefined' && l[i].leaf_disk.cryobox.preservation_method=='frozen') {
             l[i].chla_mg_g_disk_mass=l[i].chla_mg_g_disk_mass/(l[i].leaf_disk.leaf_area_and_water_sample.leaf_relative_water_content_perc/100)
             l[i].chlb_mg_g_disk_mass=l[i].chlb_mg_g_disk_mass/(l[i].leaf_disk.leaf_area_and_water_sample.leaf_relative_water_content_perc/100)
             l[i].carot_mg_g_disk_mass=l[i].carot_mg_g_disk_mass/(l[i].leaf_disk.leaf_area_and_water_sample.leaf_relative_water_content_perc/100)

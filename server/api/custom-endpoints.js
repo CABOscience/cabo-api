a898b8c96ebcf48b4df29dd00a6da21b0253cebc
Guillaume Larocque <glaroc@gmail.com>
2021-05-27 13:31:42 -0400

    Fix taxa search for permissions

--- server/api/custom-endpoints.js
+++ server/api/custom-endpoints.js
@@ -8,7 +8,7 @@ import { Parser } from 'json2csv'
 export default function (app, db) {
   //SEARCH SPECTRA BY TAXA
   app.post('/api/v1/leaf_spectra/search/taxa', function (req, res) {
-      let select = 'SELECT sample_id, b.scientific_name, o.permission FROM plants p, bulk_leaf_samples b LEFT JOIN bulk_leaf_samples b ON (p.fulcrum_id=b.plant OR p.fulcrum_id=b.plant2) LEFT JOIN projects_permissions o ON p.project=o.project';
+      let select = 'SELECT sample_id, b.scientific_name, o.permission FROM plants p LEFT JOIN bulk_leaf_samples b ON (p.fulcrum_id=b.plant OR p.fulcrum_id=b.plant2) LEFT JOIN projects_permissions o ON p.project=o.project';
       let where = ' WHERE sample_id IS NOT NULL';
       if(req.body.taxa !== ''){
         where +=" AND p.scientific_name like '%"+ req.body.taxa + "%'";

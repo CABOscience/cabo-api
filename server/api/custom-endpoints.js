010b0682bbd4d0a87c8ae041480801b4465f63fd
Guillaume Larocque <glaroc@gmail.com>
2021-08-20 15:08:03 -0400

    Change trait CSV

--- server/api/custom-endpoints.js
+++ server/api/custom-endpoints.js
@@ -288,7 +288,11 @@ export default function (app, db) {
   //TRAITS DOWNLOAD
   app.post('/api/v1/traits/csv/', function (req, res) {
     if(typeof req.body.ids !== 'undefined' && req.body.type=='raw'){
-      db.query("SELECT * FROM all_traits WHERE sample_id IN("+req.body.ids+");", { type: db.QueryTypes.SELECT }).then(result => {
+      const ids=[]
+      req.body.ids.map(r => {
+        ids.push("'"+r+"'")
+      })
+      db.query("SELECT * FROM all_traits WHERE sample_id IN("+ids+");", { type: db.QueryTypes.SELECT }).then(result => {
         try {
           const parser = new Parser();
           const csv = parser.parse(result);
@@ -298,7 +302,6 @@ export default function (app, db) {
         }
       })
     } else if(typeof req.body.taxa !== 'undefined' && req.body.type=='raw'){
-      //db.query("SELECT sample_id, scientific_name, date_measured, leaf_side_measured, wavelength, reflectance_transmittance, r_t_average from spectra_processed WHERE sample_id IN("+req.body.ids+") ORDER BY sample_id, wavelength;", { type: db.QueryTypes.SELECT }).then(result => {
       db.query("SELECT * FROM all_traits WHERE scientific_name IN("+req.body.taxa+")", { type: db.QueryTypes.SELECT }).then(result => {
         try {
           const parser = new Parser();

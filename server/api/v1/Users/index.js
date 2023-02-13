0e4a73a2e9af234d6f5bd6b49b7fa3de7c6db1a4
Guillaume Larocque <glaroc@gmail.com>
2023-02-13 12:26:32 -0500

    Small fix

--- server/api/v1/Users/index.js
+++ server/api/v1/Users/index.js
@@ -1,57 +1,61 @@
-'use strict'
+"use strict";
 
-import bcrypt from 'bcrypt'
-import _ from 'lodash'
-import { DataTypes } from 'sequelize'
+import bcrypt from "bcryptjs";
+import _ from "lodash";
+import { DataTypes } from "sequelize";
 
 export default function (db) {
-  const Users = db.define('users', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: 'uq_member_name',
-      comment: "Nom de l'utilisateur"
+  const Users = db.define(
+    "users",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: "uq_member_name",
+        comment: "Nom de l'utilisateur",
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: "uq_member_name",
+        comment: "Prénom de l'utilisateur",
+      },
+      email: {
+        type: DataTypes.STRING,
+        comment: "Adresse courriel de l'utilisateur",
+        unique: "uq_memb_email",
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      role: {
+        type: DataTypes.ENUM,
+        values: ["user", "admin"], // TODO add more values
+        defaultValue: "user",
+        comment: "role de l'utilisateur",
+        allowNull: false,
+      },
+      organization: {
+        type: DataTypes.STRING,
+        comment: "Organisme d'attache de l'utilisateur",
+      },
+      password: {
+        type: DataTypes.STRING,
+        comment: "Mot de passe de l'utilisateur",
+        allowNull: false,
+      },
     },
-    lastname: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: 'uq_member_name',
-      comment: "Prénom de l'utilisateur"
-    },
-    email: {
-      type: DataTypes.STRING,
-      comment: "Adresse courriel de l'utilisateur",
-      unique: 'uq_memb_email',
-      allowNull: false,
-      validate: {
-        isEmail: true
-      }
-    },
-    role: {
-      type: DataTypes.ENUM,
-      values: ['user', 'admin'], // TODO add more values
-      defaultValue: 'user',
-      comment: "role de l'utilisateur",
-      allowNull: false
-    },
-    organization: {
-      type: DataTypes.STRING,
-      comment: "Organisme d'attache de l'utilisateur"
-    },
-    password: {
-      type: DataTypes.STRING,
-      comment: "Mot de passe de l'utilisateur",
-      allowNull: false
+    {
+      underscored: true,
+      freezeTableName: true,
     }
-  }, {
-    underscored: true,
-    freezeTableName: true
-  })
+  );
 
   // Instance Methods
   Users.prototype.authenticate = function (value) {
-    return bcrypt.compareSync(value, this.password)
-  }
+    return bcrypt.compareSync(value, this.password);
+  };
 
-  db.Users = Users
+  db.Users = Users;
 }

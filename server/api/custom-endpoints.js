eb6c8fbf7f23861a724339639614222474c8976e
Guillaume Larocque <glaroc@gmail.com>
2023-02-16 16:06:08 -0500

    back to csv

--- server/api/custom-endpoints.js
+++ server/api/custom-endpoints.js
@@ -354,33 +354,33 @@ export default function (app, db) {
       } else {
         ids = "'" + req.body.ids + "'";
       }
-      let filename = Math.random().toString(16).slice(2) + ".csv.gz";
+      let filename = Math.random().toString(16).slice(2) + ".csv";
       if (req.body.type == "mean") {
         db.query(
           "COPY (SELECT wavelength, reflectance_transmittance, avg(r_t_average) as avg, min(r_t_average) as min, max(r_t_average) as max from spectra_processed WHERE record_id IN(" +
             ids +
-            ") GROUP BY wavelength, reflectance_transmittance ORDER BY wavelength) TO PROGRAM 'gzip > /tmp/" +
+            ") GROUP BY wavelength, reflectance_transmittance ORDER BY wavelength) TO  '/tmp/" +
             filename +
             "' DELIMITER ',' CSV HEADER;",
           { type: db.QueryTypes.SELECT }
         ).then((result) => {
           try {
-            res.status(200).send(filename);
+            res.status(200).sendFile("/tmp/" + filename);
           } catch (err) {
             console.error(err);
           }
         });
       } else if (req.body.type == "raw") {
         db.query(
-          "COPY (SELECT s.sample_id, l.site_id, l.scientific_name, s.leaf_number, l.date_measured, s.leaf_side_measured, wavelength, reflectance_transmittance, calculated_value FROM spectra_leaves s LEFT JOIN leaf_spectra l ON(s.sample_id_text=l.sample_id) WHERE s.sample_id_text IN(" +
+          "COPY (SELECT s.sample_id, l.site_id, l.scientific_name, s.leaf_number, l.date_measured, s.leaf_side_measured, wavelength, reflectance_transmittance, calculated_value FROM spectra_leaves s LEFT JOIN leaf_spectra l ON(s.sample_id_text=l.sample_id) WHERE l.sample_id_text IN(" +
             ids +
-            ") ORDER BY sample_id, leaf_number, wavelength) TO PROGRAM 'gzip > /tmp/" +
+            ") ORDER BY sample_id, leaf_number, wavelength) TO '/tmp/" +
             filename +
             "' DELIMITER ',' CSV HEADER;",
           { type: db.QueryTypes.SELECT }
         ).then((result) => {
           try {
-            res.status(200).send(filename);
+            res.status(200).sendFile("/tmp/" + filename);
           } catch (err) {
             console.error(err);
           }
@@ -401,7 +401,7 @@ export default function (app, db) {
           { type: db.QueryTypes.SELECT }
         ).then((result) => {
           try {
-            res.status(200).sendFile(filename);
+            res.status(200).sendFile("/tmp/" + filename);
           } catch (err) {
             console.error(err);
           }
